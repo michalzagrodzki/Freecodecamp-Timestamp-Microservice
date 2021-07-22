09f8e1122d8f36f564f608fe1cee9c317181f562
Michal Zagrodzki <michalzagrodzki@gmail.com>
2021-07-22 22:24:02 +0200

    Set methods for recognition of date type

--- controllers/date.js
+++ controllers/date.js
@@ -1,7 +1,18 @@
 // display date details
 exports.details = (req, res) => {
   try {
-    const setDate = req.params.date ? new Date(req.params.date) : new Date();
+    const setDateType = (date) => {
+      if (date.search("-") !== -1) return new Date(date);
+      if (new Date(parseInt(date)) !== "Invalid Date") {
+        return new Date(parseInt(date));
+      }
+    };
+    const setDefaultDate = (date) => {
+      return new Date(date);
+    };
+    const setDate = req.params.date
+      ? setDateType(req.params.date)
+      : setDefaultDate(req.params.date);
     const validatedDate = setDate.toString();
     if (validatedDate === "Invalid Date") throw validatedDate;
 
