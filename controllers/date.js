6dcaf7a88222e5e5d1199811442b01057b89dd1b
Michal Zagrodzki <michalzagrodzki@gmail.com>
2021-07-22 21:33:55 +0200

    Add method for setting default date. Add method for sending valid response. Add initial method for creating unix timestamp

--- controllers/date.js
+++ controllers/date.js
@@ -1,9 +1,19 @@
 // display date details
 exports.details = (req, res) => {
   try {
-    const validatedDate = new Date(req.params.date).toString();
+    const setDate = req.params.date ? new Date(req.params.date) : new Date();
+    const validatedDate = setDate.toString();
     if (validatedDate === "Invalid Date") throw validatedDate;
-    res.json({ date: new Date(req.params.date) });
+
+    const stringDate = setDate.toString();
+    const formattedDate = stringDate.split("+")[0];
+
+    const unixDate = setDate.getTime();
+    const response = {
+      unix: unixDate,
+      utc: formattedDate,
+    };
+    res.json(response);
   } catch (error) {
     res.json({ error: error });
   }
