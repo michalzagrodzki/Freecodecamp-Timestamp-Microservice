b41dae9112733e157489daf4c2e2638bec76fcea
Michal Zagrodzki <michalzagrodzki@gmail.com>
2021-07-23 13:05:41 +0200

    Refactor date methods

--- utils/dateMethods.js
+++ utils/dateMethods.js
@@ -1,25 +1,39 @@
 const { MONTH_NAMES } = require("./constants");
 
-// helper methods
+// date validators
+isDashDate = (date) => {
+  return date.search("-") !== -1;
+};
+isLocaleDate = (date) => {
+  return date.search(" ") !== -1;
+};
+isIntegerDate = (date) => {
+  return new Date(parseInt(date)) !== "Invalid Date";
+};
+
+// date setters
+const setDateFromDash = (date) => new Date(date);
+const setDateFromLocale = (date) => {
+  const splitDate = decodeURI(date).split(" ");
+  const dayYear = splitDate
+    .filter((item) => Number.isInteger(parseInt(item)))
+    .map((item) => parseInt(item));
+  const day = dayYear.filter((item) => item <= 12)[0];
+  const month = splitDate.filter(
+    (item) => !Number.isInteger(parseInt(item))
+  )[0];
+  const monthIndex = MONTH_NAMES.indexOf(month);
+  const year = dayYear.filter((item) => item >= 12)[0];
+  return new Date(year, monthIndex, day);
+};
+const setDateFromInteger = (date) => new Date(parseInt(date));
+
+// date public methods
 const determineDateType = (date) => {
   const setDateType = (date) => {
-    if (date.search("-") !== -1) return new Date(date);
-    if (date.search(" ") !== -1) {
-      const splitDate = decodeURI(date).split(" ");
-      const dayYear = splitDate
-        .filter((item) => Number.isInteger(parseInt(item)))
-        .map((item) => parseInt(item));
-      const day = dayYear.filter((item) => item <= 12)[0];
-      const month = splitDate.filter(
-        (item) => !Number.isInteger(parseInt(item))
-      )[0];
-      const monthIndex = MONTH_NAMES.indexOf(month);
-      const year = dayYear.filter((item) => item >= 12)[0];
-      return new Date(year, monthIndex, day);
-    }
-    if (new Date(parseInt(date)) !== "Invalid Date") {
-      return new Date(parseInt(date));
-    }
+    if (isDashDate(date)) return setDateFromDash(date);
+    if (isLocaleDate(date)) return setDateFromLocale(date);
+    if (isIntegerDate(date)) return setDateFromInteger(date);
   };
   const setDefaultDate = () => {
     return new Date();
