8956b813f24f64d63a5abb5ec6b7e7f750e3049c
Michal Zagrodzki <michalzagrodzki@gmail.com>
2021-07-23 12:03:20 +0200

    Fix failing test with month name

--- controllers/date.js
+++ controllers/date.js
@@ -1,7 +1,34 @@
 // helper methods
 const determineDateType = (date) => {
+  const MONTH_NAMES = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
   const setDateType = (date) => {
     if (date.search("-") !== -1) return new Date(date);
+    if (date.search(" ") !== -1) {
+      const splitDate = decodeURI(date).split(" ");
+      const dayYear = splitDate
+        .filter((item) => Number.isInteger(parseInt(item)))
+        .map((item) => parseInt(item));
+      const day = dayYear.filter((item) => item <= 12)[0] + 1;
+      const month = splitDate.filter(
+        (item) => !Number.isInteger(parseInt(item))
+      )[0];
+      const monthIndex = MONTH_NAMES.indexOf(month);
+      const year = dayYear.filter((item) => item >= 12)[0];
+      return new Date(year, monthIndex, day);
+    }
     if (new Date(parseInt(date)) !== "Invalid Date") {
       return new Date(parseInt(date));
     }
